7ffb64053acb587e0e870f59ea802a8922bb27f5
AntonioDattolo <antonio19794.a@gmail.com>
2024-06-04 04:08:00 +0200

    importate slide per carosello jumbo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,63 +1,19 @@
 <script>
+import myData from '../store/data';
+import LinkCard from './LinkCard.vue';
+import imgUrl from '../assets/img/h5-logo-divided-header.png'
+import iconUrl from '../assets/svg/svg-0.svg'
 
 export default {
-  
+    components :{
+    LinkCard
+    },
   data() {
+   
     return {
-        links:{ 
-            home :[
-            "Main Home",
-            "Italian Restaurant",
-            "Pizzeria Home",
-            "Pizza Light",
-            "Pizza Dark",
-            "Fast Food Home"],
-            pages :[
-                "About Us",
-                "Our Service",
-                "Our Team",
-                "Our History",
-                "Reservation",
-                "Book A Table I",
-                "Book A Table II",
-                "Contact Us",
-                "Get In Touch",
-                "Coming Soon",
-                "404 Error Page"
-            ],
-            menu:{
-                
-                list_types:["Don Peppe Menu","Menu Light","MEnu Filter Light","Menu Dark","Menu Filter Dark","Menu Slider","Restaurant Menu"],
-                list_layout:["3 Columns","4 Columns","4 Columns Wide","5 Columns","5 Columns Wide","6 Columns"],
-                product_types :["Standard","Grouped","Variable","New","On Sale"],
-                shop_pages :["My Account", "Cart", "Checkout"]
-            },
-            event:["Event List","Event Single"],
-            blog: {blog_links:[
-                "Right Sidebar",
-                "Left Sidebar",
-                "Without Sidebar",
-                "Post Types"],
-                
-                    blog_submenu: [ 
-                        "Standard",
-                        "Gallery",
-                        "Quote",
-                        "Link",
-                        "Audio",
-                        "Video",
-                        "No Sidebar"
-
-                    ]
-                
-            },
-           
-            landing :"Landing",
-            logo : "../assets/img/h5-logo-divided-header.png"
-            
-
-
-            }
+        myData,
+        imgUrl,
+        iconUrl
 
     }
   },
@@ -84,47 +40,48 @@ export default {
                     <ul class=" col-12 d-flex justify-content-around m-0" style="list-style: none;">
                         <li class=" align-self-center dropdown">
                             <a class="nav-link" aria-current="page" href="#">
-                                <img src="../assets/svg/svg-0.svg" style="height: 5px; filter: invert(1);" alt="">
+                                <img :src="iconUrl" style="height: 5px; filter: invert(1);" alt="">
                                 Home
                             </a>
-                            <ul class="dropdown-menu" >
+                            <!-- <ul class="dropdown-menu" >
                                 <li v-for="link in links.home"><a class="text-decoration-none text-black" href="#">{{ link }}</a></li>
+                            </ul> -->
+                            <ul class="dropdown-menu text-start">
+                                 <LinkCard v-for="link,i of myData.links.home" :links="link" :index="i"/> 
                             </ul>
                         </li>
                         <li class=" align-self-center dropdown">
                             <a class="nav-link" href="#">Pages</a>
                             <ul class="dropdown-menu" >
-                                <li v-for="link in links.pages"><a class="text-decoration-none text-black" href="#">{{ link }}</a></li>
+                                <LinkCard v-for="link in myData.links.pages" :links="link"/>
                             </ul>
                         </li>
                         <li class=" align-self-center dropdown">
                             <a class="nav-link" href="#">Menu</a>
                             <ul class="dropdown-menu" >
-                                <li v-for="link in links.menu.list_types"><a class="text-decoration-none text-black" href="#">{{ link }}</a></li>
+                                <LinkCard v-for="link in myData.links.menu.list_types" :links="link"/>
                             </ul>
                             
                         </li>
                         <li class="">
                             <a class="nav-link active" aria-current="page" href="#">
-                                <img src="../assets/img/h5-logo-divided-header.png" style="height: 105px;" alt="">
+                                <img :src="imgUrl" style="height: 105px;" alt="">
                             </a>
                         </li>
                         <li class=" align-self-center dropdown">
                             <a class="nav-link active" aria-current="page" href="#">Event</a>
                             <ul class="dropdown-menu" >
-                                <li v-for="link in links.event"><a class="text-decoration-none text-black" href="#">{{ link }}</a></li>
+                                <LinkCard v-for="link in myData.links.event" :links="link" />
                             </ul>
                         </li>
                         <li class=" align-self-center dropdown">
                             <a class="nav-link" href="#">Blog</a>
                             <ul class="dropdown-menu" id="blog-menu" >
-                                <li v-for="link,i in links.blog.blog_links">
-                                    <a  class="text-decoration-none text-black" href="#">{{ link }}</a>
-                                </li>
+                                <LinkCard v-for="link in myData.links.blog.blog_links" :links="link" />
                             </ul>
                         </li>
                         <li class=" align-self-center">
-                            <a class="nav-link" href="#">{{links.landing}}</a>
+                            <a class="nav-link" href="#">{{myData.links.landing}}</a>
                         </li>
                     </ul>
                 </div>
@@ -140,12 +97,22 @@ export default {
 <style scoped>
 .debug{
     border: 1px solid red;
-}.dropdown:hover>.dropdown-menu{
+
+}.pok{
+    position: relative;
+}
+.dropdown-menu{
+    display: none;
+    background-color: whitesmoke;
+    text-decoration: none;
+    list-style: none;
+  line-height-step: 0;
+    margin-left: 0;
+}
+.dropdown:hover>.dropdown-menu{
     display: block;
     position: absolute;
-    left: 0px;
-}
-#blog-menu>li:last-child::before{
-    content: 'baffo';
+    width: 180px;
+    left: 0;
 }
 </style>
--- src/components/Jumbo.vue
+++ src/components/Jumbo.vue
@@ -1,7 +1,13 @@
 <script>
 
-
-
+import myData from '../store/data';
+import background from '../assets/img/cielostellato.png'
+import slideBg_1 from '../assets/img/h3-rev-img-1.png'
+import slideBg_2 from '../assets/img/h3-rev-img-3.png'
+import slideBg_3 from '../assets/img/h3-rev-img-5.png'
+import slide_1 from '../assets/img/h3-rev-img-2.png'
+import slide_2 from '../assets/img/h3-rev-img-4.png'
+import slide_3 from '../assets/img/h3-rev-img-6.png'
 
 export default {
   components: {
@@ -9,7 +15,18 @@ export default {
   },
   data() {
     return {
-      background: "../assets/img/cielostellato.PNG"
+      myData,
+      background,
+      carousel_slide:[
+        slide_1,
+        slide_2,
+        slide_3
+      ],
+      carousel_bg:[
+      slideBg_1,
+        slideBg_2,
+        slideBg_3,
+      ]
 
     }
   },
@@ -25,20 +42,39 @@ export default {
 </script>
 
 <template>
-    <section style="width: 100%;" >
-       <img class="w-100" src="../assets/img/cielostellato.PNG" alt="">
+  <!-- impostare una variabile per swichtare le slide del carosello -->
+    <section class="d-flex justify-content-center align-items-center flex-wrap debug" v-bind:style="{ backgroundImage: 'url(' + background + ')' }">
+     <div class="p-5" style=" border: 1px solid red;">
+        <img :src="carousel_slide[0]" alt="">
+        <img style="position: absolute;left:30%;z-index:-2;top:30%" :src="carousel_bg[0]" alt="">
+     </div>
     </section>
   
 </template>
 
 <style scoped>
-img{
+.myBg{
   position: absolute;
   top:-150px;
   z-index: -1;
   object-fit: contain;
   object-position: bottom;
-  height: 750px; 
+  height: 750px;
+  width: 100%; 
+} 
+section{
+  position: absolute;
+  top :-25px;
+  z-index: -1;
+  height: 80vh;
+  width: 100%;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: bottom;
+  padding-top:10rem ;
+}
+.carousel{
+
 }
 
 </style>
